7b0f01cd2511d72ee260aeacc7ccaa3c8cf93a9b
alkmanistik <42465362+alkmanistik@users.noreply.github.com>
2025-06-30 21:35:11 +0500

    Refactor
    Удалена не использованная аннотация

--- src/main/java/com/alkmanistik/deferred_thread/data/entity/TaskEntity.java
+++ src/main/java/com/alkmanistik/deferred_thread/data/entity/TaskEntity.java
@@ -12,7 +12,6 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class TaskEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
